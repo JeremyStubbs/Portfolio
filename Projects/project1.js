a2f8a25a014cee4e95d105e59f96f74373c9b67d
JeremyStubbs <jeremyb.stubbs@gmail.com>
2021-10-27 12:50:36 -0400

    8

--- Projects/project1.js
+++ Projects/project1.js
@@ -187,6 +187,8 @@ function dragElement(elmnt) {
 	}
 }
 
+
+//
 // $(document).keypress(function (event) {
 // 	alert('Handler for .keypress() called. - ' + event.charCode);
 // });
@@ -218,3 +220,22 @@ function dragElement(elmnt) {
 // 	$('body').on('mouseup', handle_mouseup).on('mousemove', handle_dragging);
 // }
 // $('.wrapper').mousedown(handle_mousedown);
+
+
+// function handle_mousedown(e) {
+// 	window.my_dragging = {};
+// 	my_dragging.pageX0 = e.pageX;
+// 	my_dragging.pageY0 = e.pageY;
+// 	my_dragging.elem = this;
+// 	my_dragging.offset0 = $(this).offset();
+// 	function handle_dragging(e) {
+// 		var left = my_dragging.offset0.left + (e.pageX - my_dragging.pageX0);
+// 		var top = my_dragging.offset0.top + (e.pageY - my_dragging.pageY0);
+// 		$(my_dragging.elem).offset({ top: top, left: left });
+// 	}
+// 	function handle_mouseup(e) {
+// 		$('body').off('mousemove', handle_dragging).off('mouseup', handle_mouseup);
+// 	}
+// 	$('body').on('mouseup', handle_mouseup).on('mousemove', handle_dragging);
+// }
+// $('#b').mousedown(handle_mousedown);
--- Projects/project2.js
+++ Projects/project2.js
@@ -1,3 +1,4 @@
+//All this is only here for reference
 const addtodo = function (event) {
 	event.preventDefault();
 	let input = $('#input').val();
@@ -40,21 +41,3 @@ $('button').on('click', wtfmate);
 $('button').on('click', loadData);
 
 $('button').on('click', okmakessense);
-
-// function handle_mousedown(e) {
-// 	window.my_dragging = {};
-// 	my_dragging.pageX0 = e.pageX;
-// 	my_dragging.pageY0 = e.pageY;
-// 	my_dragging.elem = this;
-// 	my_dragging.offset0 = $(this).offset();
-// 	function handle_dragging(e) {
-// 		var left = my_dragging.offset0.left + (e.pageX - my_dragging.pageX0);
-// 		var top = my_dragging.offset0.top + (e.pageY - my_dragging.pageY0);
-// 		$(my_dragging.elem).offset({ top: top, left: left });
-// 	}
-// 	function handle_mouseup(e) {
-// 		$('body').off('mousemove', handle_dragging).off('mouseup', handle_mouseup);
-// 	}
-// 	$('body').on('mouseup', handle_mouseup).on('mousemove', handle_dragging);
-// }
-// $('#b').mousedown(handle_mousedown);
